1b7b39190ed01d67bd4405db9649a121f140c9bf
Gustavo Kropiwiec <59800105+gukropiwiec@users.noreply.github.com>
2020-09-13 21:22:18 -0300

    implementation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+declare var google: any;
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +9,27 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  map: any;
+
+  constructor() { }
+
+  ionViewDidEnter() {
+    this.initMap();
+  }
+
+  initMap() {
+    let coords = new google.maps.LatLng(-25.452413, -49.266977);
+    let mapOptions = {
+      center: coords,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+      
+    }
+    this.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    const marker = new google.maps.Marker({
+      position: coords,
+      map: this.map
+    });
+  }
 
 }
